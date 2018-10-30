19666561453ffdd3d242a131c13f96659769a7ca
diooo1231 <dio.snel@student.hu.nl>
2018-10-30 12:36:52 +0100

    lol

---
+++ Python/Les 4/3. Input & Output.py
@@ -0,0 +1,6 @@
+uurloon = int(input("Wat verdien je per uur"))
+aantal_uur = int(input("Hoeveel heb je gewerkt?"))
+
+TotaalLoon = aantal_uur*uurloon
+
+print((aantal_uur) , "uur werken levert" , (uurloon) , "Euro op")
---
+++ Python/Les 5/2. If with 2 boolean operators.py
@@ -0,0 +1,6 @@
+def yes_or_no(question):
+    while "Heeft u een nederlands paspoort?":
+        if reply[0] == 'y':
+            return True
+        if reply[0] == 'n':
+            return False
